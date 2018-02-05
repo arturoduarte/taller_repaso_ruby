7910487aa1f837b31d9923ee923a2440ec92b231
arturo <informa.fj@gmail.com>
2018-02-04 23:46:30 -0300

    Ejercicio1 terminado

---
+++ Objetos/ejercicio01.rb
@@ -0,0 +1,9 @@
+# Se tiene el siguiente código que no funciona, para arreglarlo se pide que el método1 funcione como método de instancia.
+
+class T
+	def self.method1()
+		puts "Probando 1 2 3"
+	end
+end
+
+T.method1
